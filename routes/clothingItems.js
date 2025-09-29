4593221209f2f333ae2da1559a186814f8065a57
linknn <crysnnelson@hotmail.com>
2025-09-28 22:20:30 -0700

    :bug:Fix clothing item route

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
-const { getItems, createItem, deleteItem } = require("../controllers/users");
+const {
+  getItems,
+  createItem,
+  deleteItem,
+} = require("../controllers/clothingItems");
 
 router.get("/", getItems);
 router.post("/", createItem);
