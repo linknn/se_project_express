354af7ff2da374208d087190f83731950bbdeb1f
linknn <crysnnelson@hotmail.com>
2025-10-02 17:39:44 -0700

    Add 409 and 401 errors

--- utils/errors.js
+++ utils/errors.js
@@ -1,8 +1,12 @@
 const BAD_REQUEST = 400;
 // message: "Invalid request",
 
+const UNAUTHORIZED = 401;
+
 const NOT_FOUND = 404;
 // message: "Resource not found",
+
+const CONFLICT = 409;
 
 const INTERNAL_SERVER_ERROR = 500;
 // message: "Something went wrong on the server",
@@ -21,4 +25,6 @@ module.exports = {
   INTERNAL_SERVER_ERROR,
   NOT_FOUND,
   orFailWithNotFound,
+  CONFLICT,
+  UNAUTHORIZED,
 };
