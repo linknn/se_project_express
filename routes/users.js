313140a7feb86852de336f7c2bbcf3b02a0899b1
linknn <crysnnelson@hotmail.com>
2025-10-02 19:06:12 -0700

    Remove routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const { getUsers, getUser, createUser } = require("../controllers/users");
+// const { getUsers, getUser, createUser } = require("../controllers/users");
 
-router.get("/", getUsers);
-router.get("/:userId", getUser);
-router.post("/", createUser);
+// Remove for sp13
+// router.get("/", getUsers);
+// router.get("/:userId", getUser);
+// router.post("/", createUser);
 
 module.exports = router;
