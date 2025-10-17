b677c6bbf693fd46ffb3848697a1c2c4a9a58763
linknn <crysnnelson@hotmail.com>
2025-10-17 09:55:20 -0700

    Remove public item route

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,6 @@ const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
 const auth = require("../middlewares/auth");
 
-// public route - GET /items
-router.use("/items", itemRouter);
-
 // protects routes below this
 router.use(auth);
 
